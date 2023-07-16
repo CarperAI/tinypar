a328edfd5af4e9c2ef1e0f32ebdaa74e2bfaef3f
Reshinth <reshinth@ip-26-0-152-143.us-west-2.compute.internal>
2023-07-16 21:13:23 +0000

    Rm

--- tinypar/model_utils.py
+++ tinypar/model_utils.py
@@ -1,1 +1,35 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.parallel import DistributedDataParallel as DDP
+
+from apex.transformer import parallel_state
+from apex.transformer import tensor_parallel
+from apex.transformer.pipeline_parallel import get_forward_backward_func, build_model
+from apex.transformer.pipeline_parallel.utils import (
+    average_losses_across_data_parallel_group,
+    setup_microbatch_calculator,
+    _reconfigure_microbatch_calculator,
+)
+
+from apex.contrib.optimizers.distributed_fused_adam import DistributedFusedAdam
+from apex.optimizers.fused_adam import FusedAdam
+
+
+import torch._dynamo
+
+
+
+class RMSNorm(torch.nn.Module):
+    def __init__(self, dim: int, eps: float = 1e-6, dtype=torch.float32):
+        super().__init__()
+        self.eps = torch.tensor(eps)
+        self.weight = nn.Parameter(torch.ones(dim, dtype=dtype))
+
+    @torch.compile
+    def _norm(self, x, eps, weight):
+        out = x * torch.rsqrt(x.float().pow(2).mean(-1, keepdim=True) + eps).type_as(x)
+        return out * weight
+
+    def forward(self, x):
+        return self._norm(x, self.eps, self.weight)
